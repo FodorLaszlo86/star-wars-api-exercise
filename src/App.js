43905be84e9142eb51fd534973ff7969d3bda9d9
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-02-13 10:44:39 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import StarWarsPeople from './StarWarsPeople';
 import './App.css';
 
 class App extends Component {
+  state = {
+    people: []
+  }
+
+
+  componentDidMount = () => {
+    fetch('https://swapi.co/api/people')
+    .then(response => response.json())
+    .then(data => this.setState({ people: data.results }))
+    .catch(() => console.log('FAILED TO LOAD YOUR DATA SIR'))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
