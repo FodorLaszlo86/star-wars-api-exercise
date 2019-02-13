71d90376596246f587fbdd7d23521e6f2dbd8bb8
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-02-13 11:16:28 +0100

    style added

--- src/StarWarsPeople.js
+++ src/StarWarsPeople.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './StarWarsPeople.css';
 
 const starWarsPeople = ({name, height, mass, birthDate }) => {
     return (
-        <div>
+        <div className='card'>
             <h2>{ name }</h2>
             <p>Height: { height }</p>
             <p>Mass: { mass }</p>
