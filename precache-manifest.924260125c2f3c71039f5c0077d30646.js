e05687d1812d36178f1a487d13ed59e78760bb9f
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-02-15 11:41:25 +0100

    Updates

---
+++ precache-manifest.924260125c2f3c71039f5c0077d30646.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93ba3070e6f3443c327d",
+    "url": "/star-wars-api-exercise/static/js/runtime~main.93ba3070.js"
+  },
+  {
+    "revision": "b067b7dc0247b09dc191",
+    "url": "/star-wars-api-exercise/static/js/main.b067b7dc.chunk.js"
+  },
+  {
+    "revision": "0f0f1ed395de9ee32ca5",
+    "url": "/star-wars-api-exercise/static/js/2.0f0f1ed3.chunk.js"
+  },
+  {
+    "revision": "b067b7dc0247b09dc191",
+    "url": "/star-wars-api-exercise/static/css/main.bb133203.chunk.css"
+  },
+  {
+    "revision": "f1846d09e6ffacc353cd923c5683b40d",
+    "url": "/star-wars-api-exercise/index.html"
+  }
+];
